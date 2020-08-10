5c970226984113758bf137ccfa62f5a17f1a2e86
Juan Huttemann <juanfhuttemann@gmail.com>
2020-08-10 18:32:35 -0400

    :white_check_mark: improve test

--- memdev_test.go
+++ memdev_test.go
@@ -6,7 +6,7 @@ func TestInfo(t *testing.T) {
 	memInfo, err := Info()
 	if err != nil {
 		t.Fatalf("Expected nil error but got %v", err)
-	
+	}
 	if len(memInfo) < 1 {
 		t.Fatalf("Expected Memory > 0, but got %d", len(memInfo))
 	}
