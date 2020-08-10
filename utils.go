6bdf6e22ef3a138c160bdfc927a859cf3471e9e3
Juan Huttemann <juanfhuttemann@gmail.com>
2020-08-10 11:10:19 -0400

    :recycle: minimal refactoring

---
+++ utils.go
@@ -0,0 +1,53 @@
+package memdev
+
+func formFactorType(ff int) string {
+	return [...]string{"",
+		"Other",
+		"Unknown",
+		"SIMM",
+		"SIP",
+		"Chip",
+		"DIP",
+		"ZIP",
+		"Proprietary Card",
+		"DIMM",
+		"TSOP",
+		"Row of Chips",
+		"RIMM",
+		"SODIMM",
+		"SRIMM",
+		"FBDIMM"}[ff]
+}
+
+func memoryType(mt int) string {
+	return [...]string{"",
+		"Other",
+		"Unknown",
+		"DRAM",
+		"EDRAM",
+		"VRAM",
+		"SRAM",
+		"RAM",
+		"ROM",
+		"FLASH",
+		"EEPROM",
+		"FEPROM",
+		"EPROM",
+		"CDRAM",
+		"3DRAM",
+		"SDRAM",
+		"SGRAM",
+		"RDRAM",
+		"DDR",
+		"DDR2 FB-DIMM",
+		"Reserved",
+		"Reserved",
+		"Reserved",
+		"DDR3",
+		"FBD2",
+		"DDR4",
+		"LPDDR",
+		"LPDDR2",
+		"LPDDR3",
+		"LPDDR4"}[mt]
+}
